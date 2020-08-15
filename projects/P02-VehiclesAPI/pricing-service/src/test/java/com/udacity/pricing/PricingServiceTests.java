a343d40a5eeb3d0fca92ee1c6b9a79c3eefd2653
Sudhir Vaidya <v.sudhir89@gmail.com>
2020-08-15 15:30:21 -0700

    Fixing Pricing service tests

--- projects/P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceTests.java
+++ projects/P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceTests.java
@@ -7,6 +7,7 @@ import com.udacity.pricing.domain.price.Price;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -17,13 +18,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class PricingServiceIntegrationTests {
+@AutoConfigureMockMvc
+public class PricingServiceTests {
 
     @LocalServerPort
     private int port;
 
     @Autowired
     private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void contextLoads() {
+
+    }
 
     @Test
     public void getPriceForVehicleId2() {
